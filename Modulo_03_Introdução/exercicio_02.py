28a5e5de6800f546e7384518c11dda8852186d8f
Zarckranos <matheusalmeida4100@gmail.com>
2023-01-25 18:33:48 -0300

    Exercicio de calculo de IMC

---
+++ Modulo_03_Introdução/exercicio_02.py
@@ -0,0 +1,12 @@
+# O exercicio consiste em calcular o IMC baseado nos dados abaixo ===
+nome   = 'Matheus Almeida'
+altura = 1.65
+peso   = 52.0
+imc    = ...
+# ===================================================================
+
+# Calculado dividindo o peso (em kg) pela altura ao quadrado (em metros).
+imc = peso / (altura ** altura)
+
+print(nome + " possui " + str(altura) + " de altura e pesa " + str(peso) + "kg")
+print("IMC:", imc)
